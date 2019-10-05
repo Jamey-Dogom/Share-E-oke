9c3d9902d596e65cfc4773294c50eb52581df68d
Hector Lopez <thebomblopez@gmail.com>
2019-10-04 20:56:55 -0700

    Routes up

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SearchComponent } from './search/search.component';
+import { PlayingComponent } from './playing/playing.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: WelcomeComponent,
+  },
+  {
+    path: ':room/playing',
+    component: PlayingComponent,
+  },
+  {
+    path: ':room/search',
+    component: SearchComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
