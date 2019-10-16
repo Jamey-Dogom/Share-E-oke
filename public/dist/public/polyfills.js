3cf3d507d8429b0b3b0dd0b7c3d280264d78b390
Hector Lopez <tblopez@pop-os.localdomain>
2019-10-16 12:16:15 -0700

    fix sideNav

--- public/dist/public/polyfills.js
+++ public/dist/public/polyfills.js
@@ -6370,8 +6370,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/jameydogom/Documents/codingdojo/share-E-oke/public/src/polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! /Users/jameydogom/Documents/codingdojo/share-E-oke/public/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! /home/tblopez/Documents/mean_projects/Share-E-oke/public/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /home/tblopez/Documents/mean_projects/Share-E-oke/public/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
